b21e7b2401167642df50dc826edfd3527fb27ab1
Md Fakrul Islam <contact.fakrul@gmail.com>
2016-08-15 13:07:03 +0600

    fix jquery wrapper in custom.js file

--- js/custom.js
+++ js/custom.js
@@ -1,46 +1,37 @@
-/**	
-	* Theme Name: WpF Authority
-	* Theme Scripts
-	* Author: WpFreeware
-	* Author URI: http://wpfreeware.com
-**/
+(function ($) {
+	
+	/* ----------------------------------------------------------- */
+	/*  WOW animation
+	/* ----------------------------------------------------------- */
+	wow = new WOW(
+				  {
+					animateClass: 'animated',
+					offset:       100
+				  }
+				);
+	wow.init();
 
-/* ----------------------------------------------------------- */
-/*  WOW animation
-/* ----------------------------------------------------------- */
-wow = new WOW(
-			  {
-				animateClass: 'animated',
-				offset:       100
-			  }
-			);
-wow.init();
-// document.getElementById('moar').onclick = function() {
-//   var section = document.createElement('section');
-//   section.className = 'section--purple wow fadeInDown';
-//   this.parentNode.insertBefore(section, this);
-// };
-
-
-/* ----------------------------------------------------------- */
-/*  Pushdown content area if header image not exist
-/* ----------------------------------------------------------- */ 
+	/* ----------------------------------------------------------- */
+	/*  Pushdown content area if header image not exist
+	/* ----------------------------------------------------------- */ 
+	
+	
+	if ($('#imgbanner').length > 0){  // return's true if element is present
+	   	$('#blogArchive').addClass('blogArchive_pushup');
+	}
+	else {
+	 	$('#blogArchive').addClass('blogArchive_pushdown');
+	}
 
 
-if (jQuery('#imgbanner').length > 0){  // return's true if element is present
-   jQuery('#blogArchive').addClass('blogArchive_pushup');
-}
-else {
- jQuery('#blogArchive').addClass('blogArchive_pushdown');
-}
-
-
-/* ----------------------------------------------------------- */
-/*  Hover dropdown menu
-/* ----------------------------------------------------------- */ 
-   
-  jQuery('ul.nav li.dropdown').hover(function() {
-      jQuery(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(200);
-    }, function() {
-      jQuery(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(200);
-    });
+	/* ----------------------------------------------------------- */
+	/*  Hover dropdown menu
+	/* ----------------------------------------------------------- */ 
+	   
+	$('ul.nav li.dropdown').hover(function() {
+	  $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(200);
+	}, function() {
+	  $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(200);
+	});
+	    
+}(jQuery));	    
